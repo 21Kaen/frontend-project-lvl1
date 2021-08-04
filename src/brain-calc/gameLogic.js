5f42d8f32cd7a17183439c6775e5028dc72f43f6
Kirill Chirkov <kchirkov21012000@gmail.com>
2021-08-04 23:16:56 +0300

    Step 6 fix linter

--- src/brain-calc/gameLogic.js
+++ src/brain-calc/gameLogic.js
@@ -5,16 +5,15 @@ export const question = () => {
   const num1 = getRandomInt(1, 50);
   const num2 = getRandomInt(1, 50);
   const oper = getRandomInt(0, 3);
-  switch (oper) {
-    case 0:
-      console.log(`Question: ${num1} + ${num2}`);
-      return num1 + num2;
-    case 1:
-      console.log(`Question: ${num1} - ${num2}`);
-      return num1 - num2;
-    case 2:
-      console.log(`Question: ${num1} * ${num2}`);
-      return num1 * num2;
+  if (oper === 0) {
+    console.log(`Question: ${num1} + ${num2}`);
+    return num1 + num2;
   }
-}
+  if (oper === 1) {
+    console.log(`Question: ${num1} - ${num2}`);
+    return num1 - num2;
+  }
+  console.log(`Question: ${num1} * ${num2}`);
+  return num1 * num2;
+};
 export const correct = (num) => String(num);
