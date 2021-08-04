a3fe1ee6e4ef0a6a372eda42bacf610d1a2a58c5
Kirill Chirkov <kchirkov21012000@gmail.com>
2021-08-04 21:49:22 +0300

    rename to index.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import game from '../src/interfaceLogic.js';
+import game from '../src/index.js';
 import { gameDescription, question, correct } from '../src/brain-even/gameLogic.js';
 
 game(gameDescription, question, correct);
