4edb9e1be7781bc3a4434307594f05122d022846
Kirill Chirkov <kchirkov21012000@gmail.com>
2021-08-04 23:07:59 +0300

    Step 6 random 1-50

--- src/brain-calc/gameLogic.js
+++ src/brain-calc/gameLogic.js
@@ -2,8 +2,8 @@ import getRandomInt from '../getRandomInt.js';
 
 export const gameDescription = 'What is the result of the expression?';
 export const question = () => {
-  const num1 = getRandomInt(1, 10);
-  const num2 = getRandomInt(1, 10);
+  const num1 = getRandomInt(1, 50);
+  const num2 = getRandomInt(1, 50);
   const oper = getRandomInt(0, 3);
   switch (oper) {
     case 0:
